5ae8f466d7da8d7d6c638b5698447ddb8bffa376
Steve Mokris <smokris@softpixel.com>
2018-04-06 11:20:36 -0400

    Support building on Linux.

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,6 @@
 from conans import ConanFile, tools, AutoToolsBuildEnvironment
 import os
+import platform
 
 class Libxml2Conan(ConanFile):
     name = 'libxml2'
@@ -8,6 +9,7 @@ class Libxml2Conan(ConanFile):
     package_version = '3'
     version = '%s-%s' % (source_version, package_version)
 
+    build_requires = 'llvm/3.3-5@vuo/stable'
     settings = 'os', 'compiler', 'build_type', 'arch'
     url = 'https://github.com/vuo/conan-libxml2'
     license = 'http://xmlsoft.org/FAQ.html#License'
@@ -25,41 +27,61 @@ class Libxml2Conan(ConanFile):
         tools.mkdir(self.build_dir)
         with tools.chdir(self.build_dir):
             autotools = AutoToolsBuildEnvironment(self)
+
+            # The LLVM/Clang libs get automatically added by the `requires` line,
+            # but this package doesn't need to link with them.
+            autotools.libs = ['c++abi']
+
             autotools.flags.append('-Oz')
-            autotools.flags.append('-mmacosx-version-min=10.10')
-            autotools.link_flags.append('-Wl,-install_name,@rpath/libxml2.dylib')
-            autotools.configure(configure_dir='../%s' % self.source_dir,
-                                args=['--quiet',
-                                      '--disable-static',
-                                      '--enable-ipv6=no',
-                                      '--enable-shared',
-                                      '--with-sax1',
-                                      '--with-threads',
-                                      '--with-xpath',
-                                      '--without-c14n',
-                                      '--without-debug',
-                                      '--without-ftp',
-                                      '--without-iconv',
-                                      '--without-iso8859x',
-                                      '--without-legacy',
-                                      '--without-lzma',
-                                      '--without-modules',
-                                      '--without-pattern',
-                                      '--without-push',
-                                      '--without-python',
-                                      '--without-reader',
-                                      '--without-regexps',
-                                      '--without-schemas',
-                                      '--without-schematron',
-                                      '--without-valid',
-                                      '--without-writer',
-                                      '--without-xinclude',
-                                      '--prefix=%s' % os.getcwd()])
-            autotools.make(args=['install'])
+
+            if platform.system() == 'Darwin':
+                autotools.flags.append('-mmacosx-version-min=10.10')
+                autotools.link_flags.append('-Wl,-install_name,@rpath/libxml2.dylib')
+
+            env_vars = {
+                'CC' : self.deps_cpp_info['llvm'].rootpath + '/bin/clang',
+                'CXX': self.deps_cpp_info['llvm'].rootpath + '/bin/clang++',
+            }
+            with tools.environment_append(env_vars):
+                autotools.configure(configure_dir='../%s' % self.source_dir,
+                                    args=['--quiet',
+                                          '--disable-static',
+                                          '--enable-ipv6=no',
+                                          '--enable-shared',
+                                          '--with-sax1',
+                                          '--with-threads',
+                                          '--with-xpath',
+                                          '--without-c14n',
+                                          '--without-debug',
+                                          '--without-ftp',
+                                          '--without-iconv',
+                                          '--without-iso8859x',
+                                          '--without-legacy',
+                                          '--without-lzma',
+                                          '--without-modules',
+                                          '--without-pattern',
+                                          '--without-push',
+                                          '--without-python',
+                                          '--without-reader',
+                                          '--without-regexps',
+                                          '--without-schemas',
+                                          '--without-schematron',
+                                          '--without-valid',
+                                          '--without-writer',
+                                          '--without-xinclude',
+                                          '--prefix=%s' % os.getcwd()])
+                autotools.make(args=['install'])
 
     def package(self):
+        if platform.system() == 'Darwin':
+            libext = 'dylib'
+        elif platform.system() == 'Linux':
+            libext = 'so'
+        else:
+            raise Exception('Unknown platform "%s"' % platform.system())
+
         self.copy('*.h', src='%s/include/libxml2' % self.build_dir, dst='include')
-        self.copy('libxml2.dylib', src='%s/lib' % self.build_dir, dst='lib')
+        self.copy('libxml2.%s' % libext, src='%s/lib' % self.build_dir, dst='lib')
 
         self.copy('%s.txt' % self.name, src=self.source_dir, dst='license')
 
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,16 +1,27 @@
 from conans import ConanFile
+import platform
 
 class Libxml2TestConan(ConanFile):
+    requires = 'llvm/3.3-5@vuo/stable'
     generators = 'qbs'
 
     def build(self):
         self.run('qbs -f "%s"' % self.source_folder)
 
     def imports(self):
-        self.copy('*.dylib', dst='bin', src='lib')
+        self.copy('*', src='bin', dst='bin')
+        self.copy('*', dst='lib', src='lib')
 
     def test(self):
-        self.run('qbs run')
+        self.run('qbs run -f "%s"' % self.source_folder)
 
-        # Ensure we only link to system libraries.
-        self.run('! (otool -L bin/libxml2.dylib | tail +3 | egrep -v "^\s*(/usr/lib/|/System/)")')
+        # Ensure we only link to system libraries and our own libraries.
+        if platform.system() == 'Darwin':
+            self.run('! (otool -L lib/libxml2.dylib | grep -v "^lib/" | egrep -v "^\s*(/usr/lib/|/System/|@rpath/)")')
+            self.run('! (otool -L lib/libxml2.dylib | fgrep "libstdc++")')
+            self.run('! (otool -l lib/libxml2.dylib | grep -A2 LC_RPATH | cut -d"(" -f1 | grep "\s*path" | egrep -v "^\s*path @(executable|loader)_path")')
+        elif platform.system() == 'Linux':
+            self.run('! (ldd lib/libxml2.so | grep -v "^lib/" | grep "/" | egrep -v "(\s(/lib64/|(/usr)?/lib/x86_64-linux-gnu/)|test_package/build)")')
+            self.run('! (ldd lib/libxml2.so | fgrep "libstdc++")')
+        else:
+            raise Exception('Unknown platform "%s"' % platform.system())
